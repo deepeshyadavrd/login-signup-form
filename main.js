e09891bf68910da34c01aa3f4c3e29323b61c155
deepeshyadavrd <raoshsbh6@gmail.com>
2024-11-12 13:13:54 +0530

    color updated, js for toggling between forms

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const container = document.querySelector('.container');
 const registerBtn = document.querySelector('.register-btn');
 const loginBtn = document.querySelector('.login-btn');
 
+// register
 registerBtn.addEventListener('click', () => {
     container.classList.add('active');
 })
 
+// login
 loginBtn.addEventListener('click', () => {
     container.classList.remove('active');
 })
